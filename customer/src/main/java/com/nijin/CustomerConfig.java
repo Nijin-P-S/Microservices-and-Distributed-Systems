e58f1192df5b742461f8cfbb0b8d9e1f34157f98
Nijin P S <nijingithub@gmail.com>
2023-11-05 13:36:23 +0530

    Updated the url and added the @LoadBalanced annotation on RestTemplate bean

--- customer/src/main/java/com/nijin/CustomerConfig.java
+++ customer/src/main/java/com/nijin/CustomerConfig.java
@@ -1,5 +1,6 @@
 package com.nijin;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerConfig {
 
     @Bean
+    @LoadBalanced //Removes the ambigity in connecting to hosts in Eureka Server, will redirect to any available host
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
